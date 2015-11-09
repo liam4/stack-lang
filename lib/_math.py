271225da35d6a609bfacf27f413cfb17d2159946
liam4 <liamdraws4@gmail.com>
2015-11-09 16:39:39 -0400

    Math floor op

--- lib/_math.py
+++ lib/_math.py
@@ -8,6 +8,14 @@ def ceil(interpeter, stack, scopes, stream):
     stack.append(tok)
 
 
+def floor(interpeter, stack, scopes, stream):
+    val1 = stack.pop()
+    val = math.floor(val1.VAL)
+    tok = interpeter.Token(TYPE="num", VAL=val)
+    stack.append(tok)
+
+
 module = {
-    "ceil": ceil
+    "ceil": ceil,
+    "floor": floor
 }
